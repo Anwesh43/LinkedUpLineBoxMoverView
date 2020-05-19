e644fda467d3328d24bcfb3680123d9072b11a8c
Anwesh43 <anweshthecool0@gmail.com>
2020-05-20 02:17:03 +0530

    adding ULBMNode class

--- uplineboxmoverview/src/main/java/com/anwesh/uiprojects/uplineboxmoverview/UpLineBoxMoverView.kt
+++ uplineboxmoverview/src/main/java/com/anwesh/uiprojects/uplineboxmoverview/UpLineBoxMoverView.kt
@@ -117,4 +117,46 @@ class UpLineBoxMoverView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class ULBMNode(var i : Int, val state : State = State()) {
+
+        private var next : ULBMNode? = null
+        private var prev : ULBMNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = ULBMNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawULBMNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : ULBMNode {
+            var curr : ULBMNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
