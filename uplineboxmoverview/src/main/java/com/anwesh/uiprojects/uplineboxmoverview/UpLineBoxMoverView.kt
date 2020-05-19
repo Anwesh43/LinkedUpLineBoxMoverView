bf2941a15eca0ad291fa9bc6c19e6da8f5634fae
Anwesh43 <anweshthecool0@gmail.com>
2020-05-20 02:08:56 +0530

    adding UpLineBoxMoverView class

--- uplineboxmoverview/src/main/java/com/anwesh/uiprojects/uplineboxmoverview/UpLineBoxMoverView.kt
+++ uplineboxmoverview/src/main/java/com/anwesh/uiprojects/uplineboxmoverview/UpLineBoxMoverView.kt
@@ -54,3 +54,19 @@ fun Canvas.drawULBMNode(i : Int, scale : Float, paint : Paint) {
     drawUpLineBoxMover(scale, size, w, paint)
     restore()
 }
+
+class UpLineBoxMoverView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
