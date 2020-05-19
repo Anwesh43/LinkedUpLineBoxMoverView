6054d812f033c300d93fcc560f1acda7e9ed5f5c
Anwesh43 <anweshthecool0@gmail.com>
2020-05-20 02:21:58 +0530

    adding UpLineBoxMover class

--- uplineboxmoverview/src/main/java/com/anwesh/uiprojects/uplineboxmoverview/UpLineBoxMoverView.kt
+++ uplineboxmoverview/src/main/java/com/anwesh/uiprojects/uplineboxmoverview/UpLineBoxMoverView.kt
@@ -159,4 +159,28 @@ class UpLineBoxMoverView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class UpLineBoxMover(var i : Int) {
+
+        private val root : ULBMNode = ULBMNode(0)
+        private var curr : ULBMNode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
