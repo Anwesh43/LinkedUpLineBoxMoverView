e6ef4cb4ce4956f18749c7ff0e5ae9e615673624
Anwesh43 <anweshthecool0@gmail.com>
2020-05-20 02:31:22 +0530

    adding Renderer in view

--- uplineboxmoverview/src/main/java/com/anwesh/uiprojects/uplineboxmoverview/UpLineBoxMoverView.kt
+++ uplineboxmoverview/src/main/java/com/anwesh/uiprojects/uplineboxmoverview/UpLineBoxMoverView.kt
@@ -57,14 +57,16 @@ fun Canvas.drawULBMNode(i : Int, scale : Float, paint : Paint) {
 
 class UpLineBoxMoverView(ctx : Context) : View(ctx) {
 
-    override fun onDraw(canvas : Canvas) {
+    private val renderer : Renderer = Renderer(this)
 
+    override fun onDraw(canvas : Canvas) {
+        renderer.render(canvas)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
@@ -188,8 +190,9 @@ class UpLineBoxMoverView(ctx : Context) : View(ctx) {
 
         private val animator : Animator = Animator(view)
         private val ulbm : UpLineBoxMover = UpLineBoxMover(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
 
-        fun render(canvas : Canvas, paint : Paint) {
+        fun render(canvas : Canvas) {
             canvas.drawColor(backColor)
             ulbm.draw(canvas, paint)
             animator.animate {
