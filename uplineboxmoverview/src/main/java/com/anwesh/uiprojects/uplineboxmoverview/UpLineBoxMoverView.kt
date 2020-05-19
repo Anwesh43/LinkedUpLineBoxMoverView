2be0db17952ee8157f50f7eeaf08466d84506be0
Anwesh43 <anweshthecool0@gmail.com>
2020-05-20 01:50:10 +0530

    adding Int and Float extension methods

--- uplineboxmoverview/src/main/java/com/anwesh/uiprojects/uplineboxmoverview/UpLineBoxMoverView.kt
+++ uplineboxmoverview/src/main/java/com/anwesh/uiprojects/uplineboxmoverview/UpLineBoxMoverView.kt
@@ -21,3 +21,8 @@ val scGap : Float = 0.02f
 val foreColor : Int = Color.parseColor("#3F51B5")
 val backColor : Int = Color.parseColor("#BDBDBD")
 val parts : Int = 3
+
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
